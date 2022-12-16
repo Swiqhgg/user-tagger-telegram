f943c944073b76031e14905b991fe0eac7e2fdb0
Jolly Mate <111024435+jollymate@users.noreply.github.com>
2022-12-17 01:03:34 +0300

    Initial commit

---
+++ start.py
@@ -0,0 +1,69 @@
+from pyrogram import Client, enums
+import random
+import time
+
+
+api_id = 25983686
+api_hash = "d49ffa3e2b617c66250b7f4c169d1cb9"
+chat_name = input("Set chat name:\n")
+
+def get_list(f_name):
+    return [row for row in open(f_name, encoding="utf-8").read().split("\n") if row]
+
+app = Client(
+    "alice",
+    api_id=api_id, api_hash=api_hash
+)
+
+app.start()
+dialogs = [dialog for dialog in app.get_dialogs(limit=20)]
+x = 0
+for dialog in dialogs:
+    print(f"{x}. chat_id: {dialog.chat.id}\n title: {dialog.chat.title}\n members count: {dialog.chat.members_count}")
+    print("============================")
+    x += 1
+chat_id = dialogs[int(input())].chat.id
+print(chat_id)
+bad_list = []
+members = [member for member in app.get_chat_members(chat_id, limit=20)]
+x = 1
+while True:
+    for member in members:
+        u_info = member.user
+        print(f"{x}. user_id: {u_info.id}\n name: {u_info.first_name} {u_info.last_name}")
+        print("============================")
+        x += 1
+    x = 1
+    user_ch = int(input("\n0. Continue\n"))
+    if user_ch == 0:
+        break
+    else:
+        bad_id = members[user_ch-1].user.id
+        bad_list.append(bad_id)
+    print(bad_list)
+
+words = get_list("sentences.txt")
+push_text = get_list("push_text.txt")
+chat_check = list(range(10))
+
+while True:
+    try:
+        word = random.choice(words)
+        push = random.choice(push_text)
+        bad_id = random.choice(bad_list)
+        msg = f"<a href='tg://user?id={bad_id}'>{push}</a> {word}"
+        ch = random.choice(chat_check)
+        if ch == 5:
+            chat = app.get_chat(chat_id)
+            title = chat.title
+            if title != chat_name:
+                app.set_chat_title(chat_id, chat_name)
+                app.delete_chat_photo(chat_id)
+        app.send_chat_action(chat_id, enums.ChatAction.TYPING)
+        time.sleep(random.randint(5, 10))
+        app.send_message(chat_id, msg, parse_mode=enums.ParseMode.HTML)
+    except:
+        pass
+    
+    
+app.stop()
